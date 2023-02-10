1e8dbfb9caf9c936533d38805acf526637b3767e
samanth kumar <samanthkumarsg@gmail.com>
2023-02-10 21:32:31 +0100

    Changed config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    SHEET_ID: '1a8kxJ5MZa0C4n5fzfe1pY6LTf5iE4MNza4eVw-jHr2Y'
-  }
+  reactStrictMode: false,
 }
 
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 import JobCard from 'components/JobCard'
 import Header from 'components/Header'
 import { google } from 'googleapis'
